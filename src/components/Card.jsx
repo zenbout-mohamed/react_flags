8ffd1098c02f1fe244d3884e55185557c0a1d9d6
zenbout-mohamed <mohamed.zenbout@laplateforme.io>
2026-04-14 11:20:59 +0200

    composant card crée

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card({ country}){
+    return (
+        <div className="card">
+            <img src={country.flags.png} alt={country.name.common}></img>
+            <h3>{country.name.common}</h3>
+            <p>Capitale : {country.capital?.[0] || "N/A"}</p>
+            <p>Region : {country.region}</p> 
+        </div>
+    );
+}
