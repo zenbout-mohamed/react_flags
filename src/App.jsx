688f5173b1341885985c80704e51f294c25e4982
zenbout-mohamed <mohamed.zenbout@laplateforme.io>
2026-04-14 13:17:53 +0200

    Filtrage réalisé

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,25 @@ export default function App (){
 
 
 //  Appels de l'API
-
 useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all")
     .then(res => setCountries(res.data))
     .catch(err => console.log(err));
 }, []);
+
+// Filtrage
+const filteredCountries = countries.filter((country) => {
+  const matchRegion = region ? country.region === region : true;
+  const matchSearch = country.name.common 
+  .toLowerCase()
+  .includes(search.toLowerCase());
+
+  return matchRegion && matchSearch;
+});
+
+return(
+  
+);
 
 
 
